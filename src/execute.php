acf6d6f64263a5b1f91f9980c810130d798fd952
sciangulahugo <sciangulahugo@gmail.com>
2024-09-23 20:18:09 -0300

    v1.0.0

--- src/execute.php
+++ src/execute.php
@@ -3,14 +3,13 @@
 $autoload = "/autoload.php";
 
 if (file_exists(dirname(__DIR__, 1) . "/vendor/" . $autoload)) {
-    require_once dirname(__DIR__, 1) . $autoload;
+    require_once dirname(__DIR__, 1) . "/vendor/" . $autoload;
 } else if (file_exists(dirname(__DIR__, 3) . $autoload)) {
     require_once dirname(__DIR__, 3) . $autoload;
 }
 
 if (isset($argv[1])) {
     $wrapper = unserialize(base64_decode($argv[1]));
-    // $closure = $wrapper->getClosure();
     $closure = \SciangulaHugo\Closure\ClosureWrapper::unserialize($wrapper);
     call_user_func($closure);
 }
